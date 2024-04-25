de38fb1abb6a8964dc60906bf8074d66775c3249
Ryan <371588694@qq.com>
2024-04-25 15:15:51 +0800

    Update index.js

--- module/index.js
+++ module/index.js
@@ -2,7 +2,7 @@
  * @Author: zhuzhan && zz.209218@sieyuan.com
  * @Date: 2024-04-24 09:59:30
  * @LastEditors: zhuzhan && zz.209218@sieyuan.com
- * @LastEditTime: 2024-04-25 14:59:13
+ * @LastEditTime: 2024-04-25 15:14:36
  * @FilePath: /node/low-code-api/module/index.js
  * @Description:
  *
@@ -22,12 +22,13 @@ router.get("/*", (req, res) => {
 		data: [],
 	};
 	const apiName = path.substring(1, path.length);
+	console.log(commonApi.hasOwnProperty(apiName));
 
 	if (apiName.length === 0) {
 		returnRes.status = 404;
 		returnRes.msg = "error";
 		returnRes.data = [];
-	} else if (commonApi?.[apiName]) {
+	} else if (commonApi.hasOwnProperty(apiName)) {
 		data = commonApi[apiName](query);
 		returnRes.data = data;
 	} else {
