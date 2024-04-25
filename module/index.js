2c0abd26475bd0499a483ea7c868d2a353a05642
Ryan <371588694@qq.com>
2024-04-25 15:05:11 +0800

    Update index.js

--- module/index.js
+++ module/index.js
@@ -2,7 +2,7 @@
  * @Author: zhuzhan && zz.209218@sieyuan.com
  * @Date: 2024-04-24 09:59:30
  * @LastEditors: zhuzhan && zz.209218@sieyuan.com
- * @LastEditTime: 2024-04-25 11:01:50
+ * @LastEditTime: 2024-04-25 14:59:13
  * @FilePath: /node/low-code-api/module/index.js
  * @Description:
  *
@@ -16,19 +16,24 @@ const router = express.Router();
 router.get("/*", (req, res) => {
 	const { path, query } = req;
 
-	let data = [];
-	const apiName = path.substring(1, path.length);
-	if (commonApi?.[apiName]) {
-		data = commonApi[apiName](query);
-	} else {
-		data = [];
-	}
-
-	res.send({
-		status:200,
+	let returnRes = {
+		status: 200,
 		msg: "success",
-		data,
-	});
+		data: [],
+	};
+	const apiName = path.substring(1, path.length);
+
+	if (apiName.length === 0) {
+		returnRes.status = 404;
+		returnRes.msg = "error";
+		returnRes.data = [];
+	} else if (commonApi?.[apiName]) {
+		data = commonApi[apiName](query);
+		returnRes.data = data;
+	} else {
+		returnRes.data = [];
+	}
+	res.send(returnRes);
 });
 
 // router.post("/*", (req, res) => {});
